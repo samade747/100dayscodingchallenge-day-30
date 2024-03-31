b4606addeabe6e0fc5591600dd2ce35d6c4d78af
samade747 <samad.e747@gmail.com>
2024-04-01 00:01:09 +0500

    12:00Am 1 4 2024

---
+++ day30/src/Components/day30a.tsx
@@ -0,0 +1,13 @@
+// function rounds a decimal number to nearest whole number
+function roundtoNearstIntegar(num: number): number {
+    return Math.round(num);
+}
+
+
+console.log(roundtoNearstIntegar(4.6)); 
+// output 5
+
+console.log(roundtoNearstIntegar(4.4));
+// output 4
+
+
