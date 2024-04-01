c05ca0ad498b4ba4a295484d7cecf249a1aca824
samade747 <samad.e747@gmail.com>
2024-04-01 16:22:35 +0500

    422pm 1 4 2024

---
+++ day30/src/Components/day30b.tsx
@@ -0,0 +1,7 @@
+
+function convertStringToNumber(str: string): number {
+    return parseFloat(str);
+}
+
+console.log(convertStringToNumber("4.6"));
+console.log(convertStringToNumber("4"))
---
+++ day30/src/Components/day30c.tsx
@@ -0,0 +1,9 @@
+
+
+function isValueNaN(value: any): boolean{
+    return isNaN(value);
+    // check and return true if the value is NaN otherwise false
+}
+
+console.log(isValueNaN(4.6));
+console.log(isValueNaN("white"))
